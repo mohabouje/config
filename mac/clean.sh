8e58d2a7866eb143fa8b5652206c6b045a11715d
Mohammed Boujemaoui Boulaghmoudi <mohabouje@gmail.com>
2022-09-17 15:53:55 +0200

    Delete also the temporal git repositories downloaded during the installation

--- mac/clean.sh
+++ mac/clean.sh
@@ -9,11 +9,15 @@ rm ${HOME}/.nanorc
 rm ${HOME}/.p10k.zsh
 rm ${HOME}/.fzf.zsh
 rm ${HOME}/.fzf.bash
+rm ${HOME}/.zshrc.pre-oh-my-zsh
 
 # Common folders for terminal plugins
 rm -rf ${HOME}/.oh-my-zsh
 rm -rf ${HOME}/.fzf
 rm -rf ${HOME}/.zsh_sessions
+
+# Git repositories download during the installation
+rm -rf "${HOME}/.git-fuzzy"
 
 # Git configuration files
 # rm ${HOME}/.gitconfig
